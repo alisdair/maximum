a1933328dd7e4f141d177102bcd8349f929547c0
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-08-18 02:29:36 +0100

    Rounding

--- src/teletext-font-rounding-algorithm/rounding.js
+++ src/teletext-font-rounding-algorithm/rounding.js
@@ -1,14 +1,14 @@
 $(function() {
   var character = [
-    0, 0, 1, 0, 0,
-    0, 1, 0, 1, 0,
-    1, 0, 0, 0, 1,
-    1, 0, 0, 0, 1,
-    1, 1, 1, 1, 1,
-    1, 0, 0, 0, 1,
-    1, 0, 0, 0, 1,
-    0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
   ];
 
   function setPixel(element, c) {
@@ -18,37 +18,67 @@ $(function() {
       $(element).attr('class', '');
     }
   }
-
-  function updateOutputForIndex(i, c) {
-    var row = Math.floor(i / 5);
-    var col = i % 5;
-    var rects = $('#character-output rect');
-
-    setPixel(rects[row * 20 + col * 2], c);
-    setPixel(rects[row * 20 + col * 2 + 1], c);
-    setPixel(rects[row * 20 + 10 + col * 2], c);
-    setPixel(rects[row * 20 + 10 + col * 2 + 1], c);
-  }
   
-  function setClickHandler($rect, i) {
+  function setClickHandler($rect, row, col) {
     $rect.click(function() {
-      var c = character[i];
+      var c = character[row][col];
 
-      character[i] = c = (c === 1 ? 0 : 1);
+      character[row][col] = c = (c === 1 ? 0 : 1);
 
       setPixel(this, c);
 
-      updateOutputForIndex(i, c);
+      updateOutput();
     });
   }
 
   $('#character-input rect').each(function(i) {
-    var c = character[i];
+    var row = Math.floor(i / 5);
+    var col = i % 5;
 
-    setClickHandler($(this), i);
+    setClickHandler($(this), row, col);
 
-    setPixel(this, c);
-    if (c === 1)
-      updateOutputForIndex(i, c);
+    setPixel(this, character[row][col]);
   });
+
+  function updateOutput() {
+    var output = [];
+
+    // Doubling
+    for (var i = 0; i < character.length; i++) {
+      output[i * 2] = [];
+      output[i * 2 + 1] = [];
+
+      for (var j = 0; j < character[i].length; j++) {
+        output[i * 2][j * 2] = character[i][j];
+        output[i * 2][j * 2 + 1] = character[i][j];
+        output[i * 2 + 1][j * 2] = character[i][j];
+        output[i * 2 + 1][j * 2 + 1] = character[i][j];
+      }
+    }
+
+    // Rounding
+    for (var i = 0; i < output.length - 1; i++) {
+      for (var j = 0; j < output[i].length - 1; j++) {
+        var a = output[i + 0][j + 0];
+        var b = output[i + 0][j + 1];
+        var c = output[i + 1][j + 0];
+        var d = output[i + 1][j + 1];
+
+        if (a === d && b === c && a !== b) {
+          output[i + 0][j + 0] = 1;
+          output[i + 0][j + 1] = 1;
+          output[i + 1][j + 0] = 1;
+          output[i + 1][j + 1] = 1;
+        }
+      }
+    }
+
+    $('#character-output rect').each(function(i) {
+      var row = Math.floor(i / 10);
+      var col = i % 10;
+      setPixel(this, output[row][col]);
+    });
+  }
+
+  updateOutput();
 });
